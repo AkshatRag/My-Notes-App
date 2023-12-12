1723c43cf46ff138b0150e6473e0c9da6f61d88e
AkshatRag <akshatrag030@gmail.com>
2023-12-12 21:58:28 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# My-Notes-App
+ This To-Do app is for my backend practice
---
+++ public/app.js
@@ -0,0 +1,2 @@
+// Your client-side JavaScript code goes here
+// This is where you handle DOM manipulations, form submissions, etc.
